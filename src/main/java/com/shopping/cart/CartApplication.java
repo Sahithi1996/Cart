da8dc55a53a262e6e9499caa7fa2cf1449badf0c
sahithi_tavva <tavva.sahithi@gmail.com>
2024-08-28 16:56:15 +0530

    Commit-2

--- src/main/java/com/shopping/cart/CartApplication.java
+++ src/main/java/com/shopping/cart/CartApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CartApplication {
-
+	// New Cart Application
 	public static void main(String[] args) {
+		System.out.println("First Log");
 		SpringApplication.run(CartApplication.class, args);
 	}
 
